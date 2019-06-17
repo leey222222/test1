5a1f08bf3fb5a4804969b2a92fc35993c2206817
leey222222 <51903068+leey222222@users.noreply.github.com>
2019-06-17 16:51:57 +0900

    수정

--- test1/src/test1/Hello.java
+++ test1/src/test1/Hello.java
@@ -3,6 +3,10 @@ package test1;
 public class Hello {
 	public static void main(String[] args) {
 		System.out.println("hello");
+		System.out.println("world");
+		System.out.println("He's name is Tom.");
+		System.out.println("Her name is Jane.");		   
+	
 	}
 
 }
