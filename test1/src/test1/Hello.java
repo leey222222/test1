dd23e1de9122615b62b8d9173134de8dd0ce8628
lee <kenta0323@naver.com>
2019-06-18 11:17:33 +0900

    yes 추가

--- test1/src/test1/Hello.java
+++ test1/src/test1/Hello.java
@@ -7,6 +7,8 @@ public class Hello {
 		System.out.println("He's name is Tom.");
 		System.out.println("Her name is Jane.");
 		System.out.println("Good Morning");
+		System.out.println("yes");
+		
 	
 	}
 
